b72f1b6f0104fcd06a7aa2684364fa4e44d1e6f3
Matt <mattfsewell@gmail.com>
2021-01-17 14:30:38 -0500

    commit tree 95f5d358f099769b7da06d23421b9b2044e49eea

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,32 @@
+class Doctor
+    @@all = []
+    attr_reader :name
+    attr_accessor :patient
+    def initialize(name)
+        @name = name
+        save
+    end
+    def save 
+        @@all.push(self)
+    end
+    def self.all
+        @@all
+    end
+    def appointments
+        Appointment.all.select {|appointment| appointment.doctor == self}
+    end
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
 
+    def patients
+        patients = []
+        Appointment.all.each do |appointment| 
+            # binding.pry
+            if appointment.doctor == self
+                patients.push(appointment.patient)
+            end
+        end
+        patients
+    end
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,31 @@
+class Patient
+    @@all = []
+    attr_reader :name
+    def initialize(name)
+        @name = name
+        save
+    end
+    def save 
+        @@all.push(self)
+    end
+    def self.all
+        @@all
+    end
+    def appointments
+        Appointment.all.select {|appointment| appointment.patient == self}
+    end
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+    def doctors
+        doctors = []
+        Appointment.all.each do |appointment| 
+            if appointment.patient == self
+                doctors.push(appointment.doctor)
+            end
+        end
+        doctors
+    end
 
+
+end
